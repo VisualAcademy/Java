8adfbaaf5c5214c154732179d4cd5411ddfe90ab
WebCamp <webcamp@outlook.kr>
2019-07-28 22:32:51 +0900

    Initial Create

---
+++ Java/HelloWorldWithVisualStudioCode/HelloWorldWIthVisualStudioCode.java
@@ -0,0 +1,9 @@
+/**
+ * HelloWorldWIthVisualStudioCode
+ */
+public class HelloWorldWIthVisualStudioCode {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Word With Visual Studio Code");
+    }
+}
