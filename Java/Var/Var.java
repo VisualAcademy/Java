c6f80a4e959056966019334b33a1833fc9a95aed
RedPlus <redplus@redplus.net>
2019-07-29 19:28:06 +0900

    기존 강의 소스 중복 저장

---
+++ Java/Var/Var.java
@@ -0,0 +1,8 @@
+public class Var {
+    public static void main(String[] args) {
+        // System.out.println("Java 10");
+
+        var name = "Java"; // String
+        System.out.println(name);
+    }
+}
