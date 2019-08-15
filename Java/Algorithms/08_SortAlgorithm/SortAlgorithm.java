98bd725b66e79aa0ee4a1d5abb31d12b4ea48e88
RedPlus <redplus@redplus.net>
2019-08-15 11:10:08 +0900

    강의 소스 추가

--- Java/Algorithms/08_SortAlgorithm/SortAlgorithm.java
+++ Java/Algorithms/08_SortAlgorithm/SortAlgorithm.java
@@ -8,19 +8,19 @@ public class SortAlgorithm {
         //[1] Input: Data Structure(Array, List, Stack, Queue, Tree, DB, ...)
         int[] data = { 3, 2, 1, 5, 4 };
         int N = data.length; // 의사코드(슈도코드) 형태로 알고리즘을 표현하기 위함
-
+        
         //[2] Process: Selection Sort(선택 정렬) 알고리즘
         for (int i = 0; i < N - 1; i++) { // i = 0 to N - 1
             for (int j = i + 1; j < N; j++) { // j = i + 1 to N
                 if (data[i] > data[j]) { // 부등호 방향: 오름차순(>), 내림차순(<)
                     int temp = data[i]; data[i] = data[j]; data[j] = temp; // SWAP
                 }
-            }
+            }   
         }
 
         //[3] Output: Console, Desktop, Web, Mobile, ...
         for (int i = 0; i < N; i++) {
-            System.out.print(data[i] + " ");
+            System.out.print(data[i] + "\t");
         }
         System.out.println();
     }
