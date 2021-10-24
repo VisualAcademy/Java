f7da77f7d3d3e76a412fa88e820ad0f8cebc0bf5
Park YongJune <redplus@redplus.net>
2021-10-25 08:25:51 +0900

    열거형

--- Java/25_Enum/EnumerationExam/EnumerationExam.java
+++ Java/25_Enum/EnumerationExam/EnumerationExam.java
@@ -1,3 +1,5 @@
+//[?] 열거형(Enumeration): 하나의 이름으로 서로 관련있는 정수 값을 갖는 상수 집합을 정의
+
 // Color 열거형
 enum Color {
     Red,
@@ -7,8 +9,8 @@ enum Color {
 
 public class EnumerationExam {
     public static void main(String[] args) {
-        System.out.println(Color.Red.ordinal());
-        System.out.println(Color.Green.ordinal());
-        System.out.println(Color.Blue.ordinal());
+        System.out.println(Color.Red.ordinal()); // 0
+        System.out.println(Color.Green.ordinal()); // 1
+        System.out.println(Color.Blue.ordinal()); // 2
     }
 }
