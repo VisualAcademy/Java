c5356213d70f6145cfa2fe88a86a12a5390299cb
Park YongJune <redplus@redplus.net>
2021-11-26 06:41:31 +0900

    IfPractice

--- Java/13_IfElse/IfPractice/IfPractice.java
+++ Java/13_IfElse/IfPractice/IfPractice.java
@@ -2,7 +2,7 @@ public class IfPractice {
     public static void main(String[] args) {
         int number = -1234;
         if (number < 0) {
-            System.out.println(number + "는 음수입니다.");
+            System.out.println(number + "은(는) 음수입니다.");
         }
     }
 }
