424e009ed0619995b9bdbbac85ca85b9d5267cf2
Park YongJune <redplus@redplus.net>
2021-12-12 12:34:06 +0900

    Code Cleanup

--- Java/21_Function/FunctionReturnValue/FunctionReturnValue.java
+++ Java/21_Function/FunctionReturnValue/FunctionReturnValue.java
@@ -1,14 +1,14 @@
 // 반환값이 있는 함수(메서드)
 public class FunctionReturnValue
 {
-    public static int SquareFunction(int x)
+    public static int squareFunction(int x)
     {
         int r = x * x;
         return r; 
     }
     public static void main(String[] args)
     {
-        int r = SquareFunction(2);
+        int r = squareFunction(2);
         System.out.println(r); // 4
     }
 }
