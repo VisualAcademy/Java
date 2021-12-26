55bc54d0c950bfcae823ec58621aed4840c98c51
Park YongJune <redplus@redplus.net>
2021-12-26 09:43:15 +0900

    .

--- Java/21_Function/GetSumTwoNumber/GetSumTwoNumber.java
+++ Java/21_Function/GetSumTwoNumber/GetSumTwoNumber.java
@@ -1,14 +1,12 @@
 // 두 수의 합을 구하는 함수(메서드)
-public class GetSumTwoNumber
-{
-    public static double GetSum(double x, double y)
-    {
+public class GetSumTwoNumber {
+    public static double getSum(double x, double y) {
         double r = x + y;
         return r; 
     }
-    public static void main(String[] args)
-    {
-        System.out.println(GetSum(3, 5));       // 8.0
-        System.out.println(GetSum(3, 0.14));    // 3.14
+
+    public static void main(String[] args) {
+        System.out.println(getSum(3, 5));       // 8.0
+        System.out.println(getSum(3, 0.14));    // 3.14
     }
 }
