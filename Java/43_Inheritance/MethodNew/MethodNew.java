71a827244a4990571e6ff23bb505b2d6bc1f7101
Park YongJune <redplus@redplus.net>
2022-01-01 20:56:58 +0900

    Code Cleanup

--- Java/43_Inheritance/MethodNew/MethodNew.java
+++ Java/43_Inheritance/MethodNew/MethodNew.java
@@ -1,18 +1,20 @@
 class Parent {
-    public void Work() {
+    public void work() {
         System.out.println("프로그래머");
     }
 }
+
 class Child extends Parent {
     // New : 새롭게 정의, 다시 정의
     @Override
-    public void Work() {
+    public void work() {
         System.out.println("프로게이머");       
     }
 }
+
 public class MethodNew {
     public static void main(String[] args) {
         Child child = new Child();
-        child.Work(); 
+        child.work(); // 프로게이머 
     }
 }
