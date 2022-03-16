b0373e28fc90daafc89f9ec110f7b05352a283fd
Park YongJune <redplus@redplus.net>
2022-03-17 00:39:42 +0900

    .

--- Java/16_Switch/SwitchStatement/FallThrough.java
+++ Java/16_Switch/SwitchStatement/FallThrough.java
@@ -12,6 +12,7 @@ public class FallThrough {
         int choice = scanner.nextInt();
         scanner.close(); 
 
+        // break 나올 때까지 실행 
         switch (choice) {
             case 1:
                 System.out.println("C 언어 선택");
@@ -19,6 +20,21 @@ public class FallThrough {
                 System.out.println("C++ 선택");
             case 3:
                 System.out.println("C# 선택");
+                break;        
+            case 4:
+                System.out.println("Java 선택");
+                break;        
+            default:
+                System.out.println("C, C++, C#, Java가 아니군요.");
+                break;
+        }
+
+        // 내용없이 하나 이상의 값과 매치될 때 실행 
+        switch (choice) {
+            case 1:
+            case 2:
+            case 3:
+                System.out.println("C 계열 선택");
                 break;        
             case 4:
                 System.out.println("Java 선택");
