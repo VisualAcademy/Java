2bfde454ef68deaa245b5cb21adc0707235bc3b4
Park YongJune <redplus@redplus.net>
2022-12-28 04:56:25 +0900

    code cleanup

--- Java/FieldInitializer/FieldInitializer.java
+++ Java/FieldInitializer/FieldInitializer.java
@@ -1,19 +1,21 @@
 class Say {
-    // 필드
+    // 필드(멤버 변수)
     private String message = "안녕하세요."; // 필드 이니셜라이저
     // 생성자
     public Say() {
         this.message = "반갑습니다."; // 생성자를 사용한 필드 초기화
     }
     // 메서드
-    public void Hi() {
+    public void hi() {
         System.out.println(this.message);
     }
 }
 
 public class FieldInitializer {
     public static void main(String[] args) {
+        // Say 클래스의 인스턴스 생성
         Say say = new Say();
-        say.Hi(); 
+        // hi() 메서드 호출
+        say.hi(); 
     }
 }
