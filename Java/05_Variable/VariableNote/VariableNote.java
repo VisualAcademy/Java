e771c2b5a194b43178a2c49cf4495acc8b135ec7
Park YongJune <redplus@redplus.net>
2023-04-03 10:41:17 +0900

    .

---
+++ Java/05_Variable/VariableNote/VariableNote.java
@@ -0,0 +1,12 @@
+public class VariableNote {
+    public static void main(String[] args) {
+        // [1] 변수 만들기(선언)
+        int i; // i 이름으로 변수 만들기
+
+        // [2] 변수에 값을 저장하기(대입, 할당)
+        i = 1234;
+
+        // [3] 변수에 들어 있는 값 사용하기(참조)
+        System.out.println(i); // 1234
+    }
+}
